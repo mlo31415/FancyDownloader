228c049c82b1bfae3221c3465b5801f060ec9de7
Mark Olson <mlo@baskerville.org>
2016-07-08 20:53:31 -0400

    Created page dowloading module

--- FancyDownloader.py
+++ FancyDownloader.py
@@ -7,7 +7,49 @@ from tkinter import filedialog
 import xml.etree.ElementTree as ET
 import os
 
+def downloadPage(pageName):
+    # Download the page's data
+    api = client.ServerProxy(url)
+    pageData=api.pages.get_one({"site" : "fancyclopedia", "page" : pageName})
 
+    # Write the page source to <pageName>.txt
+    if pageData.get("content", None) != None:
+        with open(pageName + ".txt", "w") as file:
+            print(pageData["content"], file=file)
+
+    if pageData.get("html", None) != None:
+        with open(pageName + ".html", "w") as file:
+            print(pageData["html"], file=file)
+
+    # Write the rest of the page's data to <pageName>.xml
+    root=ET.Element("data")
+    for itemName in pageData:
+        if itemName == "content" or itemName == "html":   # We've already dealt with this
+            continue
+        if itemName == "tags":
+            tags=pageData["tags"]
+            if len(tags) > 0:
+                tagsElement=ET.SubElement(root, "tags")
+                for tag in tags:
+                    tagElement=ET.SubElement(tagsElement, "tag")
+                    tagElement.text=tag
+            continue
+        if pageData[itemName] != None and pageData[itemName] != "None":
+            element=ET.SubElement(root, itemName)
+            element.text=str(pageData[itemName])
+
+        print(itemName + ": " + str(pageData[itemName]))
+
+    # And write it out.
+    tree=ET.ElementTree(root)
+    tree.write(pageName+".xml")
+
+# ---------------------------------------------
+# Main
+
+# Change the working directory to the destination of the downloaded wiki
+cwd=os.getcwd()
+os.chdir(cwd+'/site')
 
 # Write a default position file if nothing exists
 try:
@@ -20,24 +62,20 @@ except:
     tree=ET.ElementTree(root)
     tree.write("FancyDownloaderState.xml")
 
-# Open the site
+# Get the magic URL for api access
 url=open("url.txt").read()
-api = client.ServerProxy(url)
 
-# Get the page list and sort it
+# Get the page list from the wiki and sort it
+api = client.ServerProxy(url)
 listOfAllPages=api.pages.select({"site" : "fancyclopedia"})
 listOfAllPages.sort()
 
-os.chdir('site')
+# Get the page list from the downloaded directory
 
+
+# Download the pages
 for pageName in listOfAllPages:
-    api = client.ServerProxy(url)
-    pageData=api.pages.get_one({"site" : "fancyclopedia", "page" : pageName})
-    if pageData.get("content", None) != None:
-        with open(pageName + ".txt", "w") as file:
-            print(pageData["content"], file=file)
-
-    print(pageData)
+    downloadPage(pageName)
 
 print("Done")
 
