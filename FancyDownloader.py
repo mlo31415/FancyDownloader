97569b50d43f5418c2b220684ea27e973251b529
Mark Olson <mlo@baskerville.org>
2016-07-09 11:06:45 -0400

    Add code to do recent files first Fix problem printing non-standard characters

--- FancyDownloader.py
+++ FancyDownloader.py
@@ -6,20 +6,38 @@ from xmlrpc import client
 from tkinter import filedialog
 import xml.etree.ElementTree as ET
 import os
+import datetime
 
-def downloadPage(pageName):
+def DecodeDatetime(dtstring):
+    if dtstring == None:
+        return datetime.datetime(1950, 1, 1, 1, 1, 1)    # If there's no datetime, return something early
+    if not dtstring.endswith("+00:00"):
+        raise ValueError("Could not decode datetime: '")+dtstring+"'"
+    return datetime.datetime.strptime(dtstring[:-6], '%Y-%m-%dT%H:%M:%S')
+
+# Download a page from Wikidot.
+# The page's contents are stored in their files, the source in <pageName>.txt, the HTML in <pageName>..html, and all of the page information in <pageName>.xml
+# The return value is True when the Wikidot version of the page is newer than the local version, and False otherwise
+def DownloadPage(pageName):
     # Download the page's data
     api = client.ServerProxy(url)
     pageData=api.pages.get_one({"site" : "fancyclopedia", "page" : pageName})
 
+    # Get the updated time for the local version
+    localUpdatedTime=None
+    if os.path.isfile(pageName+".xml"):
+        tree=ET.parse(pageName+".xml")
+        doc=tree.getroot()
+        localUpdatedTime=doc.find("updated_at").text
+
     # Write the page source to <pageName>.txt
     if pageData.get("content", None) != None:
         with open(pageName + ".txt", "w") as file:
-            print(pageData["content"], file=file)
+            file.write(pageData["content"])
 
     if pageData.get("html", None) != None:
         with open(pageName + ".html", "w") as file:
-            print(pageData["html"], file=file)
+            file.write(pageData["html"])
 
     # Write the rest of the page's data to <pageName>.xml
     root=ET.Element("data")
@@ -34,6 +52,8 @@ def downloadPage(pageName):
                     tagElement=ET.SubElement(tagsElement, "tag")
                     tagElement.text=tag
             continue
+        if itemName == "updated_at":    # Save the updated time
+            wikiUpdatedTime=pageData[itemName]
         if pageData[itemName] != None and pageData[itemName] != "None":
             element=ET.SubElement(root, itemName)
             element.text=str(pageData[itemName])
@@ -43,6 +63,13 @@ def downloadPage(pageName):
     # And write it out.
     tree=ET.ElementTree(root)
     tree.write(pageName+".xml")
+
+
+    tWiki=DecodeDatetime(wikiUpdatedTime)
+    tLocal=DecodeDatetime(localUpdatedTime)
+
+    return tWiki>tLocal
+
 
 # ---------------------------------------------
 # Main
@@ -83,11 +110,16 @@ listOfAllDeletedPages = [val for val in listOfAllDirPages if val not in listOfAl
 # Now, get as much as possible of the list of recently modified pages.
 api = client.ServerProxy(url)
 listOfRecentlyUpdatedPages=api.pages.select({"site" : "fancyclopedia", "order": "updated_at desc"})
-# listOfRecentlyUpdatedPages=[ii for n,ii in enumerate(list) if ii not in list[:n]] # REmove duplicates
+# listOfRecentlyUpdatedPages=[ii for n,ii in enumerate(list) if ii not in list[:n]] # Remove duplicates
 
-# Download the pages
-for pageName in listOfAllWikiPages:
-    downloadPage(pageName)
+# Download the recently updated pages until we start finding pages we already have
+for pageName in listOfRecentlyUpdatedPages:
+    if not DownloadPage(pageName):
+        break
+
+# Now download missing pages
+for pageName in listOfAllMissingPages:
+    DownloadPage(pageName)
 
 print("Done")
 
