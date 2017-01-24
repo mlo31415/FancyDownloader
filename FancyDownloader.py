651f647de22a2e04bda4a89b9d806cb5454fdf24
Mark Olson <mlo@baskerville.org>
2017-01-24 12:10:02 -0500

    Skip full downloads when local page is up-to-date Provide additional control useful for restarting after download errors Detect and handle additional download errors Improve documentation

--- FancyDownloader.py
+++ FancyDownloader.py
@@ -28,6 +28,7 @@ import xml.etree.ElementTree as ET
 import os
 import datetime
 import base64
+import time
 
 def DecodeDatetime(dtstring):
     if dtstring == None:
@@ -36,45 +37,73 @@ def DecodeDatetime(dtstring):
         raise ValueError("Could not decode datetime: '")+dtstring+"'"
     return datetime.datetime.strptime(dtstring[:-6], '%Y-%m-%dT%H:%M:%S')
 
-# Download a page from Wikidot.
-# The page's contents are stored in their files, the source in <saveName>.txt, the HTML in <saveName>..html, and all of the page information in <saveName>.xml
-# The return value is True when the Wikidot version of the page is newer than the local version, and False otherwise
-def DownloadPage(localName):
+# Download a page from Wikidot and possibly store it locally.
+# The page's contents are stored in their files, the source in <saveName>.txt, the rendered HTML in <saveName>..html, and all of the page meta information in <saveName>.xml
+# Setting skipIfNotNewer to True allows a forced downloading of the page, reghardless of whether it is already stored locally.  This is mostly useful to overwrite the hidden consequences of old sync errors
+# The return value is True when the local version of the page has been updated, and False otherwise
+def DownloadPage(localName, skipIfNotNewer):
+    time.sleep(0.05)    # Wikidot has a limit on the number of RPC calls/second.  This is to throttle the dowload to stay on the safe side.
 
     # Download the page's data
-    print("   Downloading: '" + localName + "'")
-    wikiName=localName.replace("_", ":", 1)  # Convert back to the ":" form for downloading)
-    if wikiName == "con-": # "con" is a special case since that is a reserved word in Windoes and may not be used as a filename.  We use "con-" which is not a possible wiki name, for the local name .
-        wikiName="con"
-    pageData=client.ServerProxy(url).pages.get_one({"site" : "fancyclopedia", "page" : wikiName})
+    try:
+        wikiName=localName.replace("_", ":", 1)  # The '_' is used in  place of Wikidot's ':' in page names in non-default namespaces. Convert back to the ":" form for downloading)
+        if wikiName == "con-": # "con" is a special case since that is a reserved word in Windows and may not be used as a filename.  We use "con-" which is not a possible wiki name, for the local name .
+            wikiName="con"
+        pageData=client.ServerProxy(url).pages.get_one({"site" : "fancyclopedia", "page" : wikiName})
+    except:
+        print("****Failure downloading "+localName)
+        return False # Its safest on download failure to return that nothing changed
 
-    # Get the updated time for the local version
-    localUpdatedTime=None
-    if os.path.isfile(localName+ ".xml"):
-        tree=ET.parse(localName + ".xml")
-        doc=tree.getroot()
-        localUpdatedTime=doc.find("updated_at").text
+    # NOTE: This relies on the update times stored in the local file's xml and in the wiki page's updated_at metadata
+    # It will not detect incompletely downloaded pages if the xml file exists
+    if skipIfNotNewer:
+        # Get the updated time for the local version
+        localUpdatedTime=None
+        if os.path.isfile(localName+ ".xml"):
+            tree=ET.parse(localName + ".xml")
+            doc=tree.getroot()
+            localUpdatedTime=doc.find("updated_at").text
 
-    # Write the page source to <saveName>.txt
+        # Get the wiki page's updated time
+        wikiUpdatedTime=GetPageWikiTime(localName, pageData)
+        # We return True whenever we have just downloaded a page which was already up-to-date locally
+        tWiki = DecodeDatetime(wikiUpdatedTime)
+        tLocal = DecodeDatetime(localUpdatedTime)
+
+        if tWiki <= tLocal:
+            return False
+
+    # OK, we're going to download this one
+    print("   Updating: '" + localName + "'")
+
+    # Write the page source to <localName>.txt
     if pageData.get("content", None) != None:
         with open(localName + ".txt", "wb") as file:
             file.write(pageData["content"].encode("utf8"))
 
+    # Write the page's rendered HTML to <localName>.html
     if pageData.get("html", None) != None:
         with open(localName + ".html", "wb") as file:
             file.write(pageData["html"].encode("utf8"))
 
-    # Write the rest of the page's data to <saveName>.xml
-    wikiUpdatedTime = SaveMetadata(localName, pageData)
+    # Write the rest of the page's data to <localName>.xml
+    SaveMetadata(localName, pageData)
 
     # Check for attached files
+    # If any exist, save them in a directory named <localName>
+    # If none exist, don't create the directory
+    # Note that this code does not delete the directory when previously-existing files have been deleted from the wiki page
     fileNameList=client.ServerProxy(url).files.select({"site": "fancyclopedia", "page": wikiName})
     if len(fileNameList) > 0:
         if not os.path.exists(localName):
             os.mkdir(localName)   # Create a directory for the files and metadata
             os.chmod(localName, 0o777)
         for fileName in fileNameList:
-            fileStuff = client.ServerProxy(url).files.get_one({"site": "fancyclopedia", "page": wikiName, "file": fileName})    # Download the file's content and metadata
+            try:
+                fileStuff = client.ServerProxy(url).files.get_one({"site": "fancyclopedia", "page": wikiName, "file": fileName})    # Download the file's content and metadata
+            except client.Fault:
+                print("**** client.Fault loading "+fileName+". The file is probably too big.")
+                continue
             path=os.path.join(os.getcwd(), localName, fileName)
             content=base64.b64decode(fileStuff["content"])
             with open(path, "wb+") as file:
@@ -84,19 +113,17 @@ def DownloadPage(localName):
             del fileStuff["content"]    # We don't want to store the content in the metadata
             SaveMetadata(os.path.join(localName, fileName), fileStuff)
 
-    # We return True whenever we have just downloaded a page which was already up-to-date locally
-    tWiki=DecodeDatetime(wikiUpdatedTime)
-    tLocal=DecodeDatetime(localUpdatedTime)
 
-    return tWiki>tLocal
+    return True
 
-
+# Save the wiki page's metadata to an xml file
 def SaveMetadata(localName, pageData):
     root = ET.Element("data")
     wikiUpdatedTime = None
     for itemName in pageData:
-        if itemName == "content" or itemName == "html":  # We've already dealt with this
+        if itemName == "content" or itemName == "html":  # Skip: We've already dealt with this
             continue
+        # Page tags get handled specially
         if itemName == "tags":
             tags = pageData["tags"]
             if len(tags) > 0:
@@ -107,14 +134,22 @@ def SaveMetadata(localName, pageData):
             continue
         if itemName == "updated_at":  # Save the updated time
             wikiUpdatedTime = pageData[itemName]
+        # For all other pieces of metadata, create a subelement in the xml
         if pageData[itemName] != None and pageData[itemName] != "None":
             element = ET.SubElement(root, itemName)
             element.text = str(pageData[itemName])
 
-    # And write it out.
+    # And write the xml out to file <localName>.xml.
     tree = ET.ElementTree(root)
     tree.write(localName + ".xml")
     return wikiUpdatedTime
+
+# Get the wiki page's update time from the its metadata
+def GetPageWikiTime(localName, pageData):
+    for itemName in pageData:
+        if itemName == "updated_at":  # Save the updated time
+            return pageData[itemName]
+
 
 # ---------------------------------------------
 # Main
@@ -131,15 +166,26 @@ os.chmod(path, 0o777)
 # Now, get list of recently modified pages.  It will be ordered from most-recently-updated to least.
 print("Get list of all pages from Wikidot, sorted from most- to least-recently-updated")
 listOfAllWikiPages=client.ServerProxy(url).pages.select({"site" : "fancyclopedia", "order": "updated_at desc"})
-listOfAllWikiPages=[name.replace(":", "_", 1) for name in listOfAllWikiPages]   # replace the first ":" with "_" in all page names
+listOfAllWikiPages=[name.replace(":", "_", 1) for name in listOfAllWikiPages]   # ':' is used for non-standard namespaces. Replace the first ":" with "_" in all page names because ':' is invalid in Windows file names
 listOfAllWikiPages=[name if name != "con" else "con-" for name in listOfAllWikiPages]   # Handle the "con" special case
 
-# Download the recently updated pages until we start finding pages we already have
+# Download the recently updated pages until we start finding pages we already have the most recent version of
+#
+# stoppingCriterion controls how long the update runs
+# stoppingCriterion:
+#   >0 --> Run until we have encountered stoppingCriterion pages that don't need updates
+#   =0 --> Run through all pages
+# This is used to handle cases where the process had been interrupted before updating all changed pages.
+# In that case there is a wodge of up-to-date recently-changed pages before the pages that were past the interruption
+stoppingCriterion=100
 print("Downloading recently updated pages...")
+countUpToDatePages=0
 for pageName in listOfAllWikiPages:
-    if not DownloadPage(pageName):  # Quit as soon as we start re-loading pages which have not been updated
-        print("      Page is up-to-date. Ending downloads")
-        break
+    if not DownloadPage(pageName, True):
+        countUpToDatePages+=1
+        if stoppingCriterion > 0 and countUpToDatePages > stoppingCriterion:
+            print("      Ending downloads. " + str(stoppingCriterion) + " up-to-date pages found")
+            break
 
 # Get the page list from the local directory and use that to create lists of missing pages and deleted pages
 print("Creating list of local files")
@@ -154,12 +200,12 @@ listOfAllMissingPages = [val for val in listOfAllWikiPages if val not in listOfA
 if len(listOfAllMissingPages) == 0:
     print("   There are no missing pages")
 for pageName in listOfAllMissingPages:
-    DownloadPage(pageName)
+    DownloadPage(pageName, True)
 
 # And delete local copies of pages which have disappeared from the wiki
 # Note that we don't detect and delete local copies of attached files which have been removed from the wiki.
 print("Removing deleted pages...")
-listOfAllDeletedPages = [val for val in listOfAllDirPages if val not in listOfAllWikiPages]  # Create a list of pages which are dowloaded but not in the wiki
+listOfAllDeletedPages = [val for val in listOfAllDirPages if val not in listOfAllWikiPages]  # Create a list of pages which are downloaded but not in the wiki
 if len(listOfAllDeletedPages) == 0:
     print("   There are no pages to delete")
 for pageName in listOfAllDeletedPages:
