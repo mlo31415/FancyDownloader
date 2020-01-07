e2f545f8e6eae2ee12b86cc5b444bfde8bbca814
mlo31415 <mlo@baskerville.org>
2020-01-07 16:04:06 -0500

    Add the user-config file needed to point the downloaded at Fancy

---
+++ user-config.py
@@ -0,0 +1,8 @@
+mylang = 'fancyclopedia'#'en'
+#langs={'en' : 'fancyclopedia.org'}
+family = 'fancyclopedia'
+# usernames['fancyclopedia']['mlo'] = u'ExampleBot'
+family_files['fancyclopedia'] = 'http://fancyclopedia.org/api.php'
+usernames['fancyclopedia']['fancyclopedia'] = 'mlo'
+password_file = "user-password.py"
+
