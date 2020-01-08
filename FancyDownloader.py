471a0376686a5250dfd7a2635cde295800d033fa
mlo31415 <mlo@baskerville.org>
2020-01-08 17:27:28 -0500

    Mostly-complete conversion

--- FancyDownloader.py
+++ FancyDownloader.py
@@ -29,6 +29,7 @@ from datetime import timedelta
 from xmlrpc import client
 import xml.etree.ElementTree as ET
 import os
+import sys
 import datetime
 import base64
 import time
@@ -82,107 +83,105 @@ def DecodeDatetime(dtstring):
 
 # Download a page from Wikidot and possibly store it locally.
 # The page's contents are stored in their files, the source in <saveName>.txt, the rendered HTML in <saveName>..html, and all of the page meta information in <saveName>.xml
-# Setting skipIfNotNewer to True allows a forced downloading of the page, reghardless of whether it is already stored locally.  This is mostly useful to overwrite the hidden consequences of old sync errors
+# Setting updateAll to True allows a forced downloading of the page, reghardless of whether it is already stored locally.  This is mostly useful to overwrite the hidden consequences of old sync errors
 # The return value is True when the local version of the page has been updated, and False otherwise
-def DownloadPage(url, pageName, skipIfNotNewer):
-    time.sleep(0.05)    # Wikidot has a limit on the number of RPC calls/second.  This is to throttle the download to stay on the safe side.
+def DownloadPage(fancy, pageName, pageData, updateAll):
+    #time.sleep(0.05)    # Wikidot has a limit on the number of RPC calls/second.  This is to throttle the download to stay on the safe side.
 
-    # Download the page's data
-    try:
-        wikiName=pageName.replace("_", ":", 1)  # The '_' is used in  place of Wikidot's ':' in page names in non-default namespaces. Convert back to the ":" form for downloading)
-        if wikiName == "con-": # "con" is a special case since that is a reserved word in Windows and may not be used as a filename.  We use "con-" which is not a possible wiki name, for the local name .
-            wikiName="con"
-        pageData=client.ServerProxy(url).pages.get_one({"site" : "fancyclopedia", "page" : wikiName})
-    except:
-        print("****Failure downloading "+pageName)
-        return False # Its safest on download failure to return that nothing changed
+    page=pywikibot.Page(fancy, pageName)
+    if page.text is None or len(page.text) == 0:
+        return
 
     # NOTE: This relies on the update times stored in the local file's xml and in the wiki page's updated_at metadata
     # It will not detect incompletely downloaded pages if the xml file exists
-    if skipIfNotNewer:
+    # If we haven't selected updateAll, then check the timestamps and only update if the page on the server is newer than the local copy
+    if not updateAll:
         # Get the updated time for the local version
-        localUpdatedTime=None
         if os.path.isfile(pageName+".xml"):
+            # If no xml file exists, we want to do the download, since the local copy is missing or incomplete
             tree=ET.parse(pageName+".xml")
             doc=tree.getroot()
-            localUpdatedTime=doc.find("updated_at").text
+            localTimestamp=doc.find("timestamp").text
 
-        # Get the wiki page's updated time
-        wikiUpdatedTime=GetPageWikiTime(pageName, pageData)
-        # We return True whenever we have just downloaded a page which was already up-to-date locally
-        tWiki = DecodeDatetime(wikiUpdatedTime)
-        tLocal = DecodeDatetime(localUpdatedTime)
+            # Get the wiki page's updated time
+            wikiTimestamp=pageData["timestamp"]
 
-        if tWiki <= tLocal:
-            return False
+            # We return True whenever we have just downloaded a page which was already up-to-date locally
+            if wikiTimestamp <= localTimestamp:
+                return False
 
     # OK, we're going to download this one
     print("   Updating: '"+pageName+"'")
 
     # Write the page source to <pageName>.txt
-    if pageData.get("content", None) is not None:
+    text=page.text
+    if text is not None:
         with open(pageName+".txt", "wb") as file:
-            file.write(pageData["content"].encode("utf8"))
+            file.write(text.encode("utf8"))
+    else:
+        # If there's no text, delete any existing txt file
+        if os.path.exists(pageName+".txt"):
+            os.remove(pageName+".txt")
 
     # Write the page's rendered HTML to <pageName>.html
-    if pageData.get("html", None) is not None:
-        with open(pageName+".html", "wb") as file:
-            file.write(pageData["html"].encode("utf8"))
+    # if pageData.get("html", None) is not None:
+    #     with open(pageName+".html", "wb") as file:
+    #         file.write(pageData["html"].encode("utf8"))
 
     # Write the rest of the page's data to <pageName>.xml
-    SaveMetadata(pageName, pageData)
+    SaveMetadata(pageName, page)
 
-    # Check for attached files
-    # If any exist, save them in a directory named <pageName>
-    # If none exist, don't create the directory
-    # Note that this code does not delete the directory when previously-existing files have been deleted from the wiki page
-    fileNameList=client.ServerProxy(url).files.select({"site": "fancyclopedia", "page": wikiName})
-    downloadFailures=[]
-    if len(fileNameList) > 0:
-        if not os.path.exists(pageName):
-            os.mkdir(pageName)   # Create a directory for the files and metadata
-            os.chmod(pageName, 0o777)
-        for fileName in fileNameList:
-            try:
-                fileStuff = client.ServerProxy(url).files.get_one({"site": "fancyclopedia", "page": wikiName, "file": fileName})    # Download the file's content and metadata
-            except client.Fault:
-                print("**** client.Fault loading "+fileName+". The file is probably too big.")
-                downloadFailures.append(fileName)
-                continue
-            path=os.path.join(os.getcwd(), pageName, fileName)
-            content=base64.b64decode(fileStuff["content"])
-            with open(path, "wb+") as file:
-                file.write(content)     # Save the content as a file
+    # # Check for attached files
+    # # If any exist, save them in a directory named <pageName>
+    # # If none exist, don't create the directory
+    # # Note that this code does not delete the directory when previously-existing files have been deleted from the wiki page
+    # fileNameList=client.ServerProxy(url).files.select({"site": "fancyclopedia", "page": wikiName})
+    # downloadFailures=[]
+    # if len(fileNameList) > 0:
+    #     if not os.path.exists(pageName):
+    #         os.mkdir(pageName)   # Create a directory for the files and metadata
+    #         os.chmod(pageName, 0o777)
+    #     for fileName in fileNameList:
+    #         try:
+    #             fileStuff = client.ServerProxy(url).files.get_one({"site": "fancyclopedia", "page": wikiName, "file": fileName})    # Download the file's content and metadata
+    #         except client.Fault:
+    #             print("**** client.Fault loading "+fileName+". The file is probably too big.")
+    #             downloadFailures.append(fileName)
+    #             continue
+    #         path=os.path.join(os.getcwd(), pageName, fileName)
+    #         content=base64.b64decode(fileStuff["content"])
+    #         with open(path, "wb+") as file:
+    #             file.write(content)     # Save the content as a file
+    #
+    #         # Now save the file's metadata in an xml file named for the file
+    #         del fileStuff["content"]    # We don't want to store the file's content in the metadata since we already saved it as a file.
+    #         SaveMetadata(os.path.join(pageName, fileName), fileStuff)
 
-            # Now save the file's metadata in an xml file named for the file
-            del fileStuff["content"]    # We don't want to store the file's content in the metadata since we already saved it as a file.
-            SaveMetadata(os.path.join(pageName, fileName), fileStuff)
-
-    # Wikidot has a limit on the size of a file which can be downloaded through the API which is much smaller than the filesize limit on the site.
-    # If any files failed to download, try to download then using Selenium
-    # This should get the file, but can't get the metadata.
-    if len(downloadFailures) > 0:
-        # Instantiate the web browser Selenium will use. For now, we're opening it anew each time.
-        browser=webdriver.Firefox()
-        # Open the Fancy 3 page in the browser
-        browser.get("http://fancyclopedia.org/"+pageName+"/noredirect/t")
-        elem=browser.find_element_by_id('files-button')
-        elem.send_keys(Keys.RETURN)
-        time.sleep(0.7)  # Just-in-case
-        # wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'page-files')))
-
-        try:
-            els=browser.find_element_by_class_name("page-files").find_elements_by_tag_name("tr")
-        except:
-            print('******find_element_by_class_name("page-files").find_elements_by_tag_name("tr") failed')
-
-        for i in range(1, len(els)):
-            h=els[i].get_attribute("outerHTML")
-            url, linktext=GetHrefAndTextFromString(h)
-            if linktext in downloadFailures:
-                urllib.request.urlretrieve("http://fancyclopedia.org"+url, os.path.join(pageName, linktext))
-                print("     downloading big file "+linktext)
-        browser.close()
+    # # Wikidot has a limit on the size of a file which can be downloaded through the API which is much smaller than the filesize limit on the site.
+    # # If any files failed to download, try to download then using Selenium
+    # # This should get the file, but can't get the metadata.
+    # if len(downloadFailures) > 0:
+    #     # Instantiate the web browser Selenium will use. For now, we're opening it anew each time.
+    #     browser=webdriver.Firefox()
+    #     # Open the Fancy 3 page in the browser
+    #     browser.get("http://fancyclopedia.org/"+pageName+"/noredirect/t")
+    #     elem=browser.find_element_by_id('files-button')
+    #     elem.send_keys(Keys.RETURN)
+    #     time.sleep(0.7)  # Just-in-case
+    #     # wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'page-files')))
+    #
+    #     try:
+    #         els=browser.find_element_by_class_name("page-files").find_elements_by_tag_name("tr")
+    #     except:
+    #         print('******find_element_by_class_name("page-files").find_elements_by_tag_name("tr") failed')
+    #
+    #     for i in range(1, len(els)):
+    #         h=els[i].get_attribute("outerHTML")
+    #         url, linktext=GetHrefAndTextFromString(h)
+    #         if linktext in downloadFailures:
+    #             urllib.request.urlretrieve("http://fancyclopedia.org"+url, os.path.join(pageName, linktext))
+    #             print("     downloading big file "+linktext)
+    #     browser.close()
 
     return True
 
@@ -190,24 +189,12 @@ def DownloadPage(url, pageName, skipIfNotNewer):
 def SaveMetadata(localName, pageData):
     root = ET.Element("data")
     wikiUpdatedTime = None
-    for itemName in pageData:
-        if itemName == "content" or itemName == "html":  # Skip: We've already dealt with this
-            continue
-        # Page tags get handled specially
-        if itemName == "tags":
-            tags = pageData["tags"]
-            if len(tags) > 0:
-                tagsElement = ET.SubElement(root, "tags")
-                for tag in tags:
-                    tagElement = ET.SubElement(tagsElement, "tag")
-                    tagElement.text = tag
-            continue
-        if itemName == "updated_at":  # Save the updated time
-            wikiUpdatedTime = pageData[itemName]
-        # For all other pieces of metadata, create a subelement in the xml
-        if pageData[itemName] is not None and pageData[itemName] != "None":
-            element = ET.SubElement(root, itemName)
-            element.text = str(pageData[itemName])
+
+    ET.SubElement(root, "timestamp").text=str(pageData.latest_revision.timestamp)
+    ET.SubElement(root, "numrevisions").text=str(len(pageData._revisions))
+    ET.SubElement(root, "pageid").text=str(pageData.pageid)
+    ET.SubElement(root, "revid").text=str(pageData._revid)
+    ET.SubElement(root, "user").text=str(pageData.latest_revision.user)
 
     # And write the xml out to file <localName>.xml.
     tree = ET.ElementTree(root)
@@ -238,23 +225,23 @@ del cwd, path
 
 # Look for a file called "override.txt" -- if it exists, load those pages and do nothing else.
 # Override.txt contains a list of page names, one name per line.
-if os.path.exists("../FancyDownloader/override.txt"):
-    with open("../FancyDownloader/override.txt", "r") as file:
-        override=file.readlines()
-    override=[x.strip() for x in override]  # Remove trailing '\n'
-    # Remove duplicates;
-    nodupes=[]
-    for x in override:
-        if x not in nodupes:
-            nodupes.append(x)
-    override=nodupes
-    del nodupes, x
-    print("Downloading override pages...")
-    countDownloadedPages=0
-    for pageName in override:
-        if DownloadPage(fancy, pageName, False):
-            countDownloadedPages+=1
-    exit()
+# if os.path.exists("../FancyDownloader/override.txt"):
+#     with open("../FancyDownloader/override.txt", "r") as file:
+#         override=file.readlines()
+#     override=[x.strip() for x in override]  # Remove trailing '\n'
+#     # Remove duplicates;
+#     nodupes=[]
+#     for x in override:
+#         if x not in nodupes:
+#             nodupes.append(x)
+#     override=nodupes
+#     del nodupes, x
+#     print("Downloading override pages...")
+#     countDownloadedPages=0
+#     for pageName in override:
+#         if DownloadPage(fancy, pageName, True):
+#             countDownloadedPages+=1
+#     exit()
 
 # Now, get list of pages sorted by date of last modification
 # The strategy will be to first get a list of *all* updates sorted by date of last modification
@@ -297,8 +284,9 @@ stoppingCriterion=100
 print("Downloading recently updated pages...")
 countUpToDatePages=0
 countDownloadedPages=0
-for pageName in listOfAllWikiPages:
-    if DownloadPage(url, pageName, True):
+for page in listOfAllWikiPages:
+    pageName=page["title"]
+    if DownloadPage(fancy, pageName, page, False):
         countDownloadedPages+=1
     else:
         countUpToDatePages+=1
@@ -319,7 +307,7 @@ listOfAllMissingPages = [val for val in listOfAllWikiPages if val not in listOfA
 if len(listOfAllMissingPages) == 0:
     print("   There are no missing pages")
 for pageName in listOfAllMissingPages:
-    DownloadPage(url, pageName, True)
+    DownloadPage(fancy, pageName, False)
 
 # And delete local copies of pages which have disappeared from the wiki
 # Note that we don't detect and delete local copies of attached files which have been removed from the wiki where the wiki page remains.
