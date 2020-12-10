0e210eb3d20a0627468cdb70ccba6efd4ef72ccf
mlo31415 <mlo@baskerville.org>
2020-12-10 08:32:00 -0500

    Update for secure SSL

--- user-config.py
+++ user-config.py
@@ -2,7 +2,7 @@ mylang = 'fancyclopedia'#'en'
 #langs={'en' : 'fancyclopedia.org'}
 family = 'fancyclopedia'
 # usernames['fancyclopedia']['mlo'] = u'ExampleBot'
-family_files['fancyclopedia'] = 'http://fancyclopedia.org/api.php'
+family_files['fancyclopedia'] = 'https://fancyclopedia.org/api.php'
 usernames['fancyclopedia']['fancyclopedia'] = 'mlo'
 password_file = "user-password.py"
 
